12e347e19bf350a4f8ce0d1b252fbb42943b1ad9
Rishindu Yohan <rishinduyohan@gmail.com>
2026-05-03 19:35:05 +0530

    initial commit

---
+++ src/main/java/com/decodelabs/shoemartbackend/ShoeMartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.decodelabs.shoemartbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeMartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeMartBackendApplication.class, args);
+    }
+
+}
